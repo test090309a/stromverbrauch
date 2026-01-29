b920c5f75b983f70371ae52b1a07130f01140b7c
DTF i2u5h <132123946+test090309a@users.noreply.github.com>
2026-01-29 18:24:17 +0100

    Delete tmp directory
    cleaning

--- tmp/version.py
+++
@@ -1,6 +0,0 @@
-from nuitka.utils.WindowsResources import setupWindowsResources
-
-setupWindowsResources(
-    "stromverbrauch.exe",  # Ersetzen Sie dies durch den Namen Ihrer .exe-Datei
-    "version.rc"
-)
